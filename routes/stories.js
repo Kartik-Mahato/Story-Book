4dbc90042d7dc12753b517af2296d229136e5c7c
Kartik Mahato <kartikmahato1996@gmail.com>
2020-11-18 12:28:44 +0530

    Add files via upload

---
+++ routes/stories.js
@@ -0,0 +1,128 @@
+const express = require("express")
+const router = express.Router();
+const { ensureAuth } = require('../middlewares/auth');
+const Story = require('../model/Story');
+
+// show add page
+router.get('/add', ensureAuth, (req, res) => {
+    res.render('stories/add')
+})
+
+// process the post request
+router.post('/', ensureAuth, async (req, res) => {
+    try {
+        req.body.user = req.user.id;
+        await Story.create(req.body);
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.log(err);
+        res.render('error/5000')
+
+    }
+})
+
+// show public stories
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ status: 'public' })
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean()
+
+        res.render('stories/index', { stories })
+    } catch (err) {
+        console.error(err);
+        res.render('error/5000')
+    }
+})
+
+// show single page story
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id)
+            .populate('user')
+            .lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+        return res.render('stories/show', { story })
+    } catch (err) {
+        console.error(err)
+        res.render('error/404')
+    }
+})
+
+// show edit page
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    try {
+        const story = await Story.findOne({
+            _id: req.params.id
+        }).lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            res.render('stories/edit', { story })
+        }
+    } catch (err) {
+        console.error(err);
+        return res.render('error/5000')
+    }
+})
+
+// update story
+router.put('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id).lean()
+
+        if (!story) {
+            return res.render('errpr/404')
+        }
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true
+            })
+            res.redirect('/dashboard')
+        }
+    } catch (err) {
+        console.error(err);
+        return res.render('error/5000')
+    }
+})
+
+// delete story
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Story.remove({ _id: req.params.id })
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err);
+        return res.render('error/5000')
+    }
+})
+
+// show user stories page
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+        .populate('user')
+        .lean()
+        res.render('stories/index', { stories })
+    } catch (err) {
+        console.error(err)
+        res.render('error/5000')
+    }
+})
+
+module.exports = router;
